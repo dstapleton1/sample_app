f3705a1bc9d6afb2c90161ec17d2cc00b7ebae55
David Stapleton <dstapleton@gmail.com>
2015-07-19 22:22:55 +1000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,24 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
-
-#  get 'static_pages/help'
-#  get 'static_pages/about'
-#  get 'static_pages/contact'
+root 'static_pages#home'
+#  get 'users/new'
 
 get 'about' => 'static_pages#about'
 get 'help' => 'static_pages#help'
 get 'contact' => 'static_pages#contact'
 get 'signup'  => 'users#new'
+get 'users' => 'users#show'
+resources :users
 
 # root_path => '/'
 # root_url  -> 'http://www.example.com/'
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+# See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'static_pages#home'
+
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #  get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
